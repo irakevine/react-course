833f42898429191205de403b22839d943e87d216
irakevine <ikevine2016@gmail.com>
2023-07-03 09:25:50 +0200

    Initialize project using Create React App

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs] = useState([
+    {
+      title: "my new website",
+      body: "gtloremkngklgbvcdtterehgjdhhf",
+      author: "nevillen",
+      id: 1,
+    },
+    {
+      title: "welcome Party",
+      body: "rhtlortemmmjjhugdhtrefdfgfgdrtt5",
+      author: "renaike",
+      id: 2,
+    },
+    {
+      title: "my new website",
+      body: "hekhillouytuyuhhyriruirgrrew",
+      author: "nevie",
+      id: 3,
+    },
+  ]);
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs!" />
+    </div>
+  );
+};
+
+export default Home;
